e7155578ae1d3463178907bc6649848748958f0f
scobca <scobca18@yandex.ru>
2023-04-21 19:53:04 +0300

    change cli

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'aos/dist/aos.css';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter } from 'vue-router'
+
+const routes = [
+    // {
+    //     path: '/',
+    //     alias: '/home',
+    //     name: 'home',
+    //     component: HomeView
+    // }
+]
+
+const router = createRouter({
+    routes
+})
+
+export default router
