2d188e14b4d4f97786795e9c8734aad8e5b6d400
Vladimir <115186975+scobca@users.noreply.github.com>
2024-03-19 14:32:57 +0300

    stash commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <router-view/>
 </template>
 
+<script>
+import {Options, Vue} from "vue-class-component";
+
+@Options({
+  name: "app",
+})
+export default class App extends Vue {}
+</script>
+
 <style>
 
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({
+  name: 'HomeView',
+})
+export default class HomeView extends Vue {
+
+}
+</script>
+
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    min-height: 100vh;
+    display: flex;
+  }
+</style>
